4e638939e6fb9a8ef2c9b02c2d5d0d0912fd391e
gabrielalva <gabrielle.alva@gmail.com>
2015-03-19 01:32:47 -0600

    Continued work on the codebase with overall refactoring, creation of the API.

--- Swift-Prompts/SwiftPromptsView.swift
+++ Swift-Prompts/SwiftPromptsView.swift
@@ -9,29 +9,6 @@
 import Foundation
 import UIKit
 
-
-//Variables for the prompt with their default values
-var promptHeight : CGFloat = 197.0
-var promptWidth : CGFloat = 225.0
-var promtHeader : String = "Success"
-var promptHeaderTxtSize : CGFloat = 20.0
-var promptContentText : String = "You have successfully posted this item to your Facebook wall."
-var promptContentTxtSize : CGFloat = 18.0
-var promptTopBarVisibility : Bool = false
-var promptBottomBarVisibility : Bool = true
-var promptTopLineVisibility : Bool = true
-var promptBottomLineVisibility : Bool = false
-var promptOutlineVisibility : Bool = false
-//Colors of the items within the prompt
-var promptBackgroundColor : UIColor = UIColor.whiteColor()
-var promptHeaderBarColor : UIColor = UIColor.clearColor()
-var promptBottomBarColor : UIColor = UIColor(red: 34.0/255.0, green: 192.0/255.0, blue: 100.0/255.0, alpha: 1.0)
-var promptHeaderTxtColor : UIColor = UIColor(red: 155.0/255.0, green: 155.0/255.0, blue: 155.0/255.0, alpha: 1.0)
-var promptContentTxtColor : UIColor = UIColor(red: 155.0/255.0, green: 155.0/255.0, blue: 155.0/255.0, alpha: 1.0)
-var promptOutlineColor : UIColor = UIColor.clearColor()
-var promptTopLineColor : UIColor = UIColor(red: 155.0/255.0, green: 155.0/255.0, blue: 155.0/255.0, alpha: 1.0)
-var promptBottomLineColor : UIColor = UIColor.clearColor()
-
 class SwiftPromptsView: UIView
 {
     //Variables for the background view
@@ -43,7 +20,36 @@ class SwiftPromptsView: UIView
     var enableExtraLightEffect : Bool!      //Disables enableBlurring and enableTransparencyWithColor
     var enableDarkEffect : Bool!            //Disables enableBlurring and enableTransparencyWithColor
     
-
+    //Variables for the prompt with their default values
+    var promptHeight : CGFloat = 340.0
+    var promptWidth : CGFloat = 340.0
+    var promtHeader : String = "Success"
+    var promptHeaderTxtSize : CGFloat = 20.0
+    var promptContentText : String = "You have successfully posted this item to your Facebook wall."
+    var promptContentTxtSize : CGFloat = 18.0
+    var promptTopBarVisibility : Bool = false
+    var promptBottomBarVisibility : Bool = true
+    var promptTopLineVisibility : Bool = true
+    var promptBottomLineVisibility : Bool = false
+    var promptOutlineVisibility : Bool = false
+    
+    //Colors of the items within the prompt
+    var promptBackgroundColor : UIColor = UIColor.whiteColor()
+    var promptHeaderBarColor : UIColor = UIColor.clearColor()
+    var promptBottomBarColor : UIColor = UIColor(red: 34.0/255.0, green: 192.0/255.0, blue: 100.0/255.0, alpha: 1.0)
+    var promptHeaderTxtColor : UIColor = UIColor(red: 155.0/255.0, green: 155.0/255.0, blue: 155.0/255.0, alpha: 1.0)
+    var promptContentTxtColor : UIColor = UIColor(red: 155.0/255.0, green: 155.0/255.0, blue: 155.0/255.0, alpha: 1.0)
+    var promptOutlineColor : UIColor = UIColor.clearColor()
+    var promptTopLineColor : UIColor = UIColor(red: 155.0/255.0, green: 155.0/255.0, blue: 155.0/255.0, alpha: 1.0)
+    var promptBottomLineColor : UIColor = UIColor.clearColor()
+    
+    //Other variables
+    var enableDoubleButtons : Bool = false
+    var buttonText : String = "Post"
+    var secondButtonText : String = "Cancel"
+    var buttonColor : UIColor = UIColor(red: 155.0/255.0, green: 155.0/255.0, blue: 155.0/255.0, alpha: 1.0)
+    var secondButtonColor : UIColor = UIColor(red: 155.0/255.0, green: 155.0/255.0, blue: 155.0/255.0, alpha: 1.0)
+    
     override func drawRect(rect: CGRect)
     {
         var backgroundImage : UIImage = snapshot(self.superview)
@@ -115,10 +121,48 @@ class SwiftPromptsView: UIView
         
         //Create the prompt and assign its size and position
         var promptSize = CGRect(x: 0, y: 0, width: promptWidth, height: promptHeight)
-        var swiftPrompt = PromptBoxView(frame: promptSize)
+        var swiftPrompt = PromptBoxView(master: self)
         swiftPrompt.backgroundColor = UIColor.clearColor()
         swiftPrompt.center = CGPointMake(self.center.x, self.center.y)
-        self.addSubview(swiftPrompt)        
+        self.addSubview(swiftPrompt)
+        
+        //Add the button(s) on the bottom of the prompt
+        if (enableDoubleButtons == false)
+        {
+            let button   = UIButton.buttonWithType(UIButtonType.System) as UIButton
+            button.frame = CGRectMake(0, promptHeight-52, promptWidth, 41)
+            button.setTitleColor(buttonColor, forState: .Normal)
+            button.titleLabel!.font = UIFont(name: "HelveticaNeue-Light", size: 20)
+            button.setTitle(buttonText, forState: UIControlState.Normal)
+            button.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+    
+            swiftPrompt.addSubview(button)
+        }
+        else
+        {
+            var divider = UIView(frame: CGRectMake(promptWidth/2, promptHeight-47, 1, 31))
+            divider.backgroundColor = promptBottomLineColor
+            
+            swiftPrompt.addSubview(divider)
+            
+            let button   = UIButton.buttonWithType(UIButtonType.System) as UIButton
+            button.frame = CGRectMake(promptWidth/2, promptHeight-52, promptWidth/2, 41)
+            button.setTitleColor(buttonColor, forState: .Normal)
+            button.titleLabel!.font = UIFont(name: "HelveticaNeue-Light", size: 20)
+            button.setTitle(buttonText, forState: UIControlState.Normal)
+            button.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+            
+            swiftPrompt.addSubview(button)
+            
+            let secondButton   = UIButton.buttonWithType(UIButtonType.System) as UIButton
+            secondButton.frame = CGRectMake(0, promptHeight-52, promptWidth/2, 41)
+            secondButton.setTitleColor(secondButtonColor, forState: .Normal)
+            secondButton.titleLabel!.font = UIFont(name: "HelveticaNeue-Light", size: 20)
+            secondButton.setTitle(secondButtonText, forState: UIControlState.Normal)
+            secondButton.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+            
+            swiftPrompt.addSubview(secondButton)
+        }
         
         //Apply animation effect to present this view
         var applicationLoadViewIn = CATransition()
@@ -126,6 +170,11 @@ class SwiftPromptsView: UIView
         applicationLoadViewIn.type = kCATransitionReveal
         applicationLoadViewIn.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
         self.layer.addAnimation(applicationLoadViewIn, forKey: kCATransitionReveal)
+    }
+    
+    func buttonAction(sender:UIButton!)
+    {
+        println("Button tapped")
     }
     
     // MARK: - Helper Functions
@@ -139,7 +188,7 @@ class SwiftPromptsView: UIView
         return image;
     }
     
-    // MARK: - API Functions
+    // MARK: - API Functions For The Background
     func setBlurringLevel(level: CGFloat) { blurringLevel = level }
     func setColorWithTransparency(color: UIColor) { colorWithTransparency = color }
     func enableBlurringView () { enableBlurring = true }
@@ -148,12 +197,63 @@ class SwiftPromptsView: UIView
     func enableExtraLightEffectView () { enableLightEffect = false; enableExtraLightEffect = true; enableDarkEffect = false }
     func enableDarkEffectView () { enableLightEffect = false; enableExtraLightEffect = false; enableDarkEffect = true }
     
-}
-
-class PromptBoxView: UIView
-{
-    override func drawRect(rect: CGRect)
+    // MARK: - API Functions For The Prompt
+    func setPromptHeight (height : CGFloat) { promptHeight = height }
+    func setPromptWidth (width : CGFloat) { promptWidth = width }
+    func setPromtHeader (header : String) { promtHeader = header }
+    func setPromptHeaderTxtSize (headerTxtSize : CGFloat) { promptHeaderTxtSize = headerTxtSize }
+    func setPromptContentText (contentTxt : String) { promptContentText = contentTxt }
+    func setPromptContentTxtSize (contentTxtSize : CGFloat) { promptContentTxtSize = contentTxtSize }
+    func setPromptTopBarVisibility (topBarVisibility : Bool) { promptTopBarVisibility = topBarVisibility }
+    func setPromptBottomBarVisibility (bottomBarVisibility : Bool) { promptBottomBarVisibility = bottomBarVisibility }
+    func setPromptTopLineVisibility (topLineVisibility : Bool) { promptTopLineVisibility = topLineVisibility }
+    func setPromptBottomLineVisibility (bottomLineVisibility : Bool) { promptBottomLineVisibility = bottomLineVisibility }
+    func setPromptOutlineVisibility (outlineVisibility: Bool) { promptOutlineVisibility = outlineVisibility }
+    func setPromptBackgroundColor (backgroundColor : UIColor) { promptBackgroundColor = backgroundColor }
+    func setPromptHeaderBarColor (headerBarColor : UIColor) { promptHeaderBarColor = headerBarColor }
+    func setPromptBottomBarColor (bottomBarColor : UIColor) { promptBottomBarColor = bottomBarColor }
+    func setPromptHeaderTxtColor (headerTxtColor  : UIColor) { promptHeaderTxtColor =  headerTxtColor}
+    func setPromptContentTxtColor (contentTxtColor : UIColor) { promptContentTxtColor = contentTxtColor }
+    func setPromptOutlineColor (outlineColor : UIColor) { promptOutlineColor = outlineColor }
+    func setPromptTopLineColor (topLineColor : UIColor) { promptTopLineColor = topLineColor }
+    func setPromptBottomLineColor (bottomLineColor : UIColor) { promptBottomLineColor = bottomLineColor }
+    func enableDoubleButtonsOnPrompt () { enableDoubleButtons = true }
+    func setButtonText (buttonTitle : String) { buttonText = buttonTitle }
+    func setSecondButtonText (secondButtonTitle : String) { secondButtonText = secondButtonTitle }
+    func setButtonColor (colorForButton : UIColor) { buttonColor = colorForButton }
+    func setSecondButtonColor (colorForSecondButton : UIColor) { secondButtonColor = colorForSecondButton }
+    
+    
+    // MARK: - Create The Prompt With A UIView Sublass
+    class PromptBoxView: UIView
     {
-        SwiftPrompts.drawSwiftPrompt(frame: self.bounds, backgroundColor: promptBackgroundColor, headerBarColor: promptHeaderBarColor, bottomBarColor: promptBottomBarColor, headerTxtColor: promptHeaderTxtColor, contentTxtColor: promptContentTxtColor, outlineColor: promptOutlineColor, topLineColor: promptTopLineColor, bottomLineColor: promptBottomLineColor, promptText: promptContentText, textSize: promptContentTxtSize, topBarVisibility: promptTopBarVisibility, bottomBarVisibility: promptBottomBarVisibility, headerText: promtHeader, headerSize: promptHeaderTxtSize, topLineVisibility: promptTopLineVisibility, bottomLineVisibility: promptBottomLineVisibility, outlineVisibility: promptOutlineVisibility)
+        let masterClass : SwiftPromptsView
+        
+        init(master: SwiftPromptsView) {
+            masterClass = master
+            var promptSize = CGRect(x: 0, y: 0, width: masterClass.promptWidth, height: masterClass.promptHeight)
+            super.init(frame: promptSize)
+        }
+
+        required init(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        override func drawRect(rect: CGRect)
+        {
+            SwiftPrompts.drawSwiftPrompt(frame: self.bounds, backgroundColor: masterClass.promptBackgroundColor, headerBarColor: masterClass.promptHeaderBarColor, bottomBarColor: masterClass.promptBottomBarColor, headerTxtColor: masterClass.promptHeaderTxtColor, contentTxtColor: masterClass.promptContentTxtColor, outlineColor: masterClass.promptOutlineColor, topLineColor: masterClass.promptTopLineColor, bottomLineColor: masterClass.promptBottomLineColor, promptText: masterClass.promptContentText, textSize: masterClass.promptContentTxtSize, topBarVisibility: masterClass.promptTopBarVisibility, bottomBarVisibility: masterClass.promptBottomBarVisibility, headerText: masterClass.promtHeader, headerSize: masterClass.promptHeaderTxtSize, topLineVisibility: masterClass.promptTopLineVisibility, bottomLineVisibility: masterClass.promptBottomLineVisibility, outlineVisibility: masterClass.promptOutlineVisibility)
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- Swift-Prompts/ViewController.swift
+++ Swift-Prompts/ViewController.swift
@@ -20,7 +20,9 @@ class ViewController: UIViewController {
         backgroundImageView.contentMode = .ScaleAspectFill
     }
 
-    @IBAction func launchPrompt(sender: UIButton) {
+    @IBAction func launchPrompt(sender: UIButton)
+    {
+        //Create an instance of SwiftPromptsView in a var
         var prompt = SwiftPromptsView(frame: self.view.bounds)
         
         //Enable blurring and add its level of blur
@@ -31,15 +33,17 @@ class ViewController: UIViewController {
         prompt.enableTransparencyWithColorView()
         prompt.setColorWithTransparency(UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 0.64))
         
-        promtHeader = "Facebook"
-        promptTopBarVisibility = true
-        promptBottomBarVisibility = false
-        promptTopLineVisibility = false
-        promptBottomLineVisibility = true
-        promptHeaderBarColor = UIColor(red: 59.0/255.0, green: 89.0/255.0, blue: 152.0/255.0, alpha: 0.67)
-        promptHeaderTxtColor = UIColor.whiteColor()
-        promptContentTxtColor = UIColor(red: 59.0/255.0, green: 89.0/255.0, blue: 152.0/255.0, alpha: 0.72)
-        promptBottomLineColor = UIColor(red: 151.0/255.0, green: 151.0/255.0, blue: 151.0/255.0, alpha: 1.0)
+        //Set the properties of the promt to look as desired
+        prompt.setPromtHeader("Facebook")
+        prompt.setPromptTopBarVisibility(true)
+        prompt.setPromptBottomBarVisibility(false)
+        prompt.setPromptTopLineVisibility(false)
+        prompt.setPromptBottomLineVisibility(true)
+        prompt.setPromptHeaderBarColor(UIColor(red: 59.0/255.0, green: 89.0/255.0, blue: 152.0/255.0, alpha: 0.67))
+        prompt.setPromptHeaderTxtColor(UIColor.whiteColor())
+        prompt.setPromptContentTxtColor(UIColor(red: 59.0/255.0, green: 89.0/255.0, blue: 152.0/255.0, alpha: 0.72))
+        prompt.setPromptBottomLineColor(UIColor(red: 151.0/255.0, green: 151.0/255.0, blue: 151.0/255.0, alpha: 1.0))
+        prompt.enableDoubleButtonsOnPrompt()
         
         self.view.addSubview(prompt)
     }
