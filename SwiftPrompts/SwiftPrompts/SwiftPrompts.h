06ef8edde752b14c972a132cfbb2062b5b484565
Patrick <patrick@helmsdeep.at>
2016-03-23 09:50:39 +0100

    added new project

---
+++ SwiftPrompts/SwiftPrompts/SwiftPrompts.h
@@ -0,0 +1,19 @@
+//
+//  SwiftPrompts.h
+//  SwiftPrompts
+//
+//  Created by Patrick Steiner on 23.03.16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftPrompts.
+FOUNDATION_EXPORT double SwiftPromptsVersionNumber;
+
+//! Project version string for SwiftPrompts.
+FOUNDATION_EXPORT const unsigned char SwiftPromptsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftPrompts/PublicHeader.h>
+
+
